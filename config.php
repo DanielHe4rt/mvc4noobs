4e112ff22f9e3c44ffa82dbc2f81a69aa4751860
Daniel Reis <daniel_o_amigo@hotmail.com>
2018-02-24 13:07:40 -0300

    Criação dos arquivos base

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$config['database'] = "interno";
+$config['host']     = "localhost";
+$config['user']     = "root";
+$config['pass']     = "";
---
+++ model/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace model;
+
+class Database(){
+    public static function connect(){
+        include("config.php");
+        return  new \PDO("mysql:host=".$config['host'].";dbname=".$config['database'],$config['user'],$config['pass']);
+    }
+}
