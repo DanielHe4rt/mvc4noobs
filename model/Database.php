e8ae112ecdd8c30c380af145cb0881d1e2d00c1d
Daniel Reis <daniel_o_amigo@hotmail.com>
2018-02-24 14:38:24 -0300

    fix class db

--- model/Database.php
+++ model/Database.php
@@ -2,7 +2,7 @@
 
 namespace model;
 
-class Database(){
+class Database{
     public static function connect(){
         include("config.php");
         return  new \PDO("mysql:host=".$config['host'].";dbname=".$config['database'],$config['user'],$config['pass']);
