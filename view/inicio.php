426b8296fdd577fcd6395ca4031e0c02b73396ad
Daniel Reis <daniel_o_amigo@hotmail.com>
2018-02-23 23:38:25 -0300

    Criação dos arquivos base

--- view/inicio.php
+++ view/inicio.php
@@ -1,1 +1,14 @@
-<h1>Como aprender um MVC topp</h1>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>MVC 4 Noobs</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+    <script src="assets/js/main.js"></script>
+</head>
+<body>
+    <h1>Apenas mais um teste</h1>
+</body>
+</html>
